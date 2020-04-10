2b00c18c0f5671740851ba3a32109a38ca31b875
Porsche Rodjanasak <45418798+PorscheSE@users.noreply.github.com>
2020-04-10 14:14:19 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
 
 provider "azurerm" {
     version = "2.0.0"
-	subscription_id = "8afbe872-4126-415f-bbf5-59890b64e029"
+    subscription_id = "8afbe872-4126-415f-bbf5-59890b64e029"
     client_id       = "7838c180-37d1-4ef8-a13b-b872a00d5c96"
     client_secret   = var.client_secret
     tenant_id       = "6e06e42d-6925-47c6-b9e7-9581c7ca302a"
